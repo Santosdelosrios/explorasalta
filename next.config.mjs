3b221428b46a23c9eb61281dfc08082226eba273
Santosdelosrios <santosdelosrios12@gmail.com>
2025-11-28 02:57:08 -0300

    Remove typedRoutes from experimental config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { typedRoutes: true },
+  experimental: { },
   reactStrictMode: true,
   images: { remotePatterns: [] }
 };
