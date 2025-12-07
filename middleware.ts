ce792188db426bd7af86f37dd3b4742a82ade598
Santosdelosrios <santosdelosrios12@gmail.com>
2025-12-06 22:46:21 -0300

    Fix middleware matcher config

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import {intlMiddleware, i18nMatcher} from '@/lib/i18n/middleware';
+import {intlMiddleware} from '@/lib/i18n/middleware';
 import type {NextRequest} from 'next/server';
 
 export function middleware(request: NextRequest) {
   return intlMiddleware(request);
 }
 
-export const config = { matcher: i18nMatcher };
+export const config = { matcher: ['/', '/(es|en)/:path*'] };
