d5da27eb4248eb8fbdac1ca9fe4ba9cc1fc95869
Santosdelosrios <santosdelosrios12@gmail.com>
2025-12-06 22:37:34 -0300

    Ensure middleware exports function

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,8 @@
-export { default } from '@/lib/i18n/middleware';
-export const config = { matcher: ['/', '/(es|en)/:path*'] };
+import {intlMiddleware, i18nMatcher} from '@/lib/i18n/middleware';
+import type {NextRequest} from 'next/server';
+
+export function middleware(request: NextRequest) {
+  return intlMiddleware(request);
+}
+
+export const config = { matcher: i18nMatcher };
